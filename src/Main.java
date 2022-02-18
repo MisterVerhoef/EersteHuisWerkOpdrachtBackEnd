c57e2ab5a4d2876a9fb5a60bc8a3ff14dacc2ce0
MisterVerhoef <frankverhoef@outlook.com>
2022-02-18 13:51:05 +0100

    ruwe eindversie

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Frank");
+       String firstName = "Frank";
+       String lastName= "Verhoef";
+       int age = 42;
+       String cityOfResidence = "Oosterwijk";
+
+
+        System.out.println("Hallo, I am " + firstName + " " + lastName + ".  I'm " + age + " years of age, " + "and live in " + cityOfResidence);
 
     }
+
 }
